3b778c071a6f456fa9e733db34a2feb799aafa0b
maher <maher.popal@lumaserv.com>
2024-06-02 20:47:42 +0200

    Initial commit

---
+++ src/main/java/de/hsrm/quiz_gateway/QuizGatewayApplication.java
@@ -0,0 +1,13 @@
+package de.hsrm.quiz_gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuizGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuizGatewayApplication.class, args);
+	}
+
+}
---
+++ src/test/java/de/hsrm/quiz_gateway/QuizGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package de.hsrm.quiz_gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuizGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
