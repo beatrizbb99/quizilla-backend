c2e3cc2fd82dd250fe3c72a6524ed50091e2f18c
maher <maher.popal@lumaserv.com>
2024-06-09 00:12:14 +0200

    Add user authentication

--- src/main/java/de/hsrm/quiz_gateway/QuizGatewayApplication.java
+++ src/main/java/de/hsrm/quiz_gateway/QuizGatewayApplication.java
@@ -1,7 +1,11 @@
 package de.hsrm.quiz_gateway;
 
+import de.hsrm.quiz_gateway.model.User;
+import de.hsrm.quiz_gateway.repository.UserRepository;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class QuizGatewayApplication {
@@ -9,5 +13,4 @@ public class QuizGatewayApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(QuizGatewayApplication.class, args);
 	}
-
 }
---
+++ src/main/java/de/hsrm/quiz_gateway/controller/AuthController.java
@@ -0,0 +1,71 @@
+package de.hsrm.quiz_gateway.controller;
+
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.algorithms.Algorithm;
+import de.hsrm.quiz_gateway.model.User;
+import de.hsrm.quiz_gateway.request.ChangePasswordRequest;
+import de.hsrm.quiz_gateway.request.LoginRequest;
+import de.hsrm.quiz_gateway.request.SignupRequest;
+import de.hsrm.quiz_gateway.response.TokenResponse;
+import de.hsrm.quiz_gateway.service.UserDetailsServiceImpl;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.web.bind.annotation.*;
+
+import java.time.Instant;
+
+import static org.springframework.http.ResponseEntity.ok;
+
+@RestController
+@RequestMapping("/auth")
+public class AuthController {
+
+    private final UserDetailsServiceImpl userService;
+    private final UserDetailsService userDetailsService;
+    private final AuthenticationManager authenticationManager;
+
+
+    public AuthController(UserDetailsServiceImpl userService, UserDetailsService userDetailsService, AuthenticationManager authenticationManager) {
+        this.userService = userService;
+        this.userDetailsService = userDetailsService;
+        this.authenticationManager = authenticationManager;
+    }
+
+    @PostMapping("/register")
+    public ResponseEntity<?> registerUser(@RequestBody SignupRequest request) {
+        User savedUser = userService.save(request.getUsername(),request.getPassword(),request.getEmail());
+        return ok(savedUser);
+    }
+
+    @PostMapping("/login")
+    public ResponseEntity<?> login(@RequestBody LoginRequest request) {
+        Authentication authRequest = new UsernamePasswordAuthenticationToken(request.getUsername(), request.getPassword());
+        Authentication authResponse = this.authenticationManager.authenticate(authRequest);
+        UserDetails user = (UserDetails) authResponse.getPrincipal();
+        String token = JWT.create().withSubject(user.getUsername()).withExpiresAt(Instant.now().plusSeconds(3600)).sign(Algorithm.HMAC256("1234"));
+        ResponseEntity responseEntity = new ResponseEntity<>(token, HttpStatus.OK);
+        return responseEntity;
+    }
+
+    @PostMapping("/change-password")
+    public ResponseEntity<?> changePassword(@RequestBody ChangePasswordRequest request) {
+        UserDetails userDetails = (UserDetails) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+        //TODO
+        return ok("password changed");
+    }
+
+    @GetMapping("/self")
+    public ResponseEntity<?> getAuthenticatedUser() {
+        return new ResponseEntity<>(HttpStatus.UNAUTHORIZED);
+    }
+
+
+}
+
