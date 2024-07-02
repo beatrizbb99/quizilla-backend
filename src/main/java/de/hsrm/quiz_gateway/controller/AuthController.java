0ed7986e43dbb1b69217cb3152d5ab60268d3638
Sadia Miah <sadia.miah@student.hs-rm.de>
2024-07-02 14:40:03 +0200

    ert

--- src/main/java/de/hsrm/quiz_gateway/controller/AuthController.java
+++ src/main/java/de/hsrm/quiz_gateway/controller/AuthController.java
@@ -3,6 +3,7 @@ package de.hsrm.quiz_gateway.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
@@ -15,6 +16,7 @@ import java.io.IOException;
 import java.util.Map;
 
 @RestController
+@RequestMapping("/auth")
 public class AuthController {
 
     @Autowired
